da5255034cafac490892e003c8d9060e856d5635
Nigel van Keulen <Nigelvk@itextra.nl>
2022-10-25 22:29:35 +0200

    Updated configs

--- main.go
+++ main.go
@@ -132,6 +132,7 @@ func GetDir(name string, project_name string) (Directory, error) {
 			proj_vars := strings.Split(project_name, "\\")
 			project_name_urlomitted = proj_vars[len(proj_vars)-1]
 		}
+		project_name_urlomitted = strings.ReplaceAll(project_name_urlomitted, "-", "_")
 		file = bytes.Replace(file, []byte("$$PROJECT_NAME$$"), []byte(project_name), -1)
 		var re = regexp.MustCompile(`\$\$PROJECT_NAME\s*;\s*OMITURL\$\$`)
 		file = re.ReplaceAll(file, []byte(project_name_urlomitted))
